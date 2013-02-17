6dc58479bb46a38cc1b1c5e236fc84c4b8ebf2b9
Kota Weaver <kota.weaver@gmail.com>
2013-02-17 10:26:18 -0500

    better loading of images

--- src/parsearguments.py
+++ src/parsearguments.py
@@ -11,7 +11,14 @@ args = parser.parse_args()
 
 _pool = list()
 
+'''
 if args.openfile:
 	for i in args.openfile:
 		_pool.append(Thread(target=lambda: imageloader.load_from_uri(i)))
 		_pool[-1].start()
+'''
+
+if args.openfile:
+	for i in args.openfile:
+		imageloader.load_from_uri(i)
+	imageloader.imglist[-1].set_image()
